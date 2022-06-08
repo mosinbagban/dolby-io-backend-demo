07cf94cdf4b43d21dec23955e91ea8526dc9e476
Mohsin Bagwan <mohsin.bagwan@dolby.com>
2022-06-08 13:59:26 +0100

    MONGOPWD name changed

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,10 @@
 const MONGOUSER=process.env.MONGOUSER;
-const MONG0PWD=process.env.MONG0PWD;
+const MONGOPWD=process.env.MONGOPWD;
 module.exports = {
   // url: "mongodb://localhost:3000/dolbyio_db"
   /**
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/drivers/node/ for more details
      */
-    url: `mongodb+srv://${MONGOUSER}:${MONG0PWD}@cluster0.v3ofyot.mongodb.net/?retryWrites=true&w=majority`
+    url: `mongodb+srv://${MONGOUSER}:${MONGOPWD}@cluster0.v3ofyot.mongodb.net/?retryWrites=true&w=majority`
 };
