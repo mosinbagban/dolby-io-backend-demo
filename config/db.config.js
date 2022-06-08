0d2cfa742249b4f6ededca5432c28b197dacfd3b
Mohsin Bagwan <mohsin.bagwan@dolby.com>
2022-06-08 13:55:24 +0100

    moved the mongodb credential to config file

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,10 @@
+const MONGOUSER=process.env.MONGOUSER;
+const MONG0PWD=process.env.MONG0PWD;
 module.exports = {
   // url: "mongodb://localhost:3000/dolbyio_db"
   /**
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/drivers/node/ for more details
      */
-    url: "mongodb+srv://mosindolby:mosin8dolby@cluster0.v3ofyot.mongodb.net/?retryWrites=true&w=majority"
+    url: `mongodb+srv://${MONGOUSER}:${MONG0PWD}@cluster0.v3ofyot.mongodb.net/?retryWrites=true&w=majority`
 };
